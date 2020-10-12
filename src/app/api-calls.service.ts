6e390f8d6ea1eaf5ae0ba0f124c8227210bd7a7d
kodurianil <kanil554@gmail.com>
2020-10-12 08:59:00 +0530

    2 files modified

--- src/app/api-calls.service.ts
+++ src/app/api-calls.service.ts
@@ -6,7 +6,162 @@ import { User } from './user';
 @Injectable()
 export class ApiCallsService {
   userIdCount = 0;
-  usersList: User[] = [];
+  usersList: User[] = [
+  {
+    "firstName": "asd",
+    "lastName": "asdf",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "asf@asdmf",
+    "address": "af",
+    "pincode": 898989,
+    "id": 1
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8971335338,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 1
+    },
+    "email": "kanil554@gmail.com",
+    "address": "adf",
+    "pincode": 898989,
+    "id": 2
+  },
+  {
+    "firstName": "anil",
+    "lastName": "koduru",
+    "mobile": 8971335338,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sd",
+    "pincode": 898989,
+    "id": 3
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 4
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 5
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 6
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 7
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 8
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 9
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 10
+  },
+  {
+    "firstName": "asf",
+    "lastName": "koduru",
+    "mobile": 8989898989,
+    "dob": {
+      "year": 2020,
+      "month": 10,
+      "day": 6
+    },
+    "email": "kanil554@gmail.com",
+    "address": "sa",
+    "pincode": 909090,
+    "id": 11
+  }
+];
   users$: BehaviorSubject<User[]>;
   constructor(
     // private httpClient: HttpClient
